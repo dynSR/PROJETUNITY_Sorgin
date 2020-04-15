8e2fe61a96637763901510c4ff1710113c041202
GERARD_Maxime <maxime.gerard@e-artsup.net>
2020-04-15 19:04:41 +0200

    Création de l'UI pour le procès Mise en place d'un choix avec 3 issues : juste / faux / temps écoulé Variable qui décompte le nombre de réponses justes Mise à jour de l'UI lors d'une réponse juste

---
+++ Assets/Maxime/MyScripts/UIManagerProces.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Fungus;
+
+public class UIManagerProces : MonoBehaviour
+{
+    private int nbGoodAnswers;
+    public int nbMaxGoodAnswers;
+    public TextMeshProUGUI txtGoodAnswers;
+    public Flowchart mainFlowchart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtGoodAnswers.text = "0 / " + nbMaxGoodAnswers.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoodAnswersTxtUpdate()
+    {
+        nbGoodAnswers = mainFlowchart.GetIntegerVariable("GoodAnswers");
+        txtGoodAnswers.text = nbGoodAnswers.ToString() + " / " + nbMaxGoodAnswers.ToString();
+    }
+}
