d7d7e287e260495157c0d3c3925e22f5593c52b8
GERARD_Maxime <maxime.gerard@e-artsup.net>
2020-04-17 18:56:41 +0200

    Scene Proces Sandbox finie

--- Assets/Maxime/MyScripts/UIManagerProces.cs
+++ Assets/Maxime/MyScripts/UIManagerProces.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using Fungus;
 
 public class UIManagerProces : MonoBehaviour
 {
     private int nbGoodAnswers;
-    public int nbMaxGoodAnswers;
+    private int nbMaxGoodAnswers;
     public TextMeshProUGUI txtGoodAnswers;
+    public TextMeshProUGUI txtPointsEarned;
     public Flowchart mainFlowchart;
+    public GameObject endSection;
 
     // Start is called before the first frame update
     void Start()
     {
+        nbMaxGoodAnswers = mainFlowchart.GetIntegerVariable("MaxAnswers");
         txtGoodAnswers.text = "0 / " + nbMaxGoodAnswers.ToString();
     }
 
@@ -28,5 +32,22 @@ public class UIManagerProces : MonoBehaviour
     {
         nbGoodAnswers = mainFlowchart.GetIntegerVariable("GoodAnswers");
         txtGoodAnswers.text = nbGoodAnswers.ToString() + " / " + nbMaxGoodAnswers.ToString();
+    }
+
+    public void PointsEarned()
+    {
+        endSection.SetActive(true);
+        int pointsEarned = mainFlowchart.GetIntegerVariable("GoodAnswers") * 100;
+        txtPointsEarned.text = "Vous avez obtenu " + pointsEarned + " points de bénédiction.";
+    }
+
+    public void ButtonRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ButtonMain()
+    {
+        SceneManager.LoadScene("SBMainScene");
     }
 }
