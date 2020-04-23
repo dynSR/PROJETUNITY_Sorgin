89b8b818a16c88bbde870814c765373e2d965e67
Dylan_Schumacker <dylan.schumacker@outlook.fr>
2020-04-23 13:38:14 +0200

    - Finition sprites étourdissement, clone, input de changement de sort équipé

---
+++ Assets/Dylan/MyScripts/DebugGUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugGUI : MonoBehaviour
+{
+    [SerializeField] private Vector2 rectPosition;
+    [SerializeField] private float rectWidth = 50f;
+    [SerializeField] private float rectHeigth = 50f;
+    [Multiline]
+    [SerializeField] private string textToDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(rectPosition.x, rectPosition.y, rectWidth, rectHeigth), textToDisplay);
+    }
+}
---
+++ Assets/Dylan/MyScripts/ShopRelated/ShopManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    public GameObject[] spellsAvailableInShop;
+    public int amntOfSpellBought = 0;
+
+    public static ShopManager s_Singleton;
+    private void Awake()
+    {
+        if (s_Singleton != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            s_Singleton = this;
+
+            if (spellsAvailableInShop.Length == 0)
+                Debug.LogError("Please populate the spell avalaible in shop list !!!!!!!!!!");
+        }
+    }
+}
