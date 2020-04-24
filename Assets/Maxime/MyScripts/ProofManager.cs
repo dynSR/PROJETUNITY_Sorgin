cc165641c4cfafaac34579e53d0b7112168ba3c4
GERARD_Maxime <maxime.gerard@e-artsup.net>
2020-04-24 17:21:57 +0200

    Avant procés

---
+++ Assets/Maxime/MyScripts/ProofManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProofManager : MonoBehaviour
+{
+    public static ProofManager singleton;
+
+    public GameObject[] proofList;
+    private int activeProof;
+    private float timerSwap = 0f;
+
+    private void Awake()
+    {
+        if (singleton)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            singleton = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeProof = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timerSwap > 0)
+        {
+            timerSwap -= Time.deltaTime;
+        }
+        else
+        {
+            if (Input.GetAxis("Horizontal") < -0.1)
+            {
+                PreviousProof();
+                Debug.Log(activeProof);
+                timerSwap = 0.5f;
+                return;
+            }
+
+            if (Input.GetAxis("Horizontal") > 0.1)
+            {
+                NextProof();
+                Debug.Log(activeProof);
+                timerSwap = 0.5f;
+                return;
+            }
+        }
+    }
+
+    private void NextProof()
+    {
+        if(activeProof != proofList.Length-1)
+        {
+            activeProof += 1;
+            UIManagerAvantProces.singleton.ProofDisplayUpdate(activeProof);
+        }
+        else
+        {
+            activeProof = 0;
+            UIManagerAvantProces.singleton.ProofDisplayUpdate(activeProof);
+        }
+        
+    }
+    
+    private void PreviousProof()
+    {
+        if (activeProof != 0)
+        {
+            activeProof -= 1;
+            UIManagerAvantProces.singleton.ProofDisplayUpdate(activeProof);
+        }
+        else
+        {
+            activeProof = proofList.Length-1;
+            UIManagerAvantProces.singleton.ProofDisplayUpdate(activeProof);
+        }
+    }
+}
---
+++ Assets/Maxime/MyScripts/UIManagerAvantProces.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManagerAvantProces : MonoBehaviour
+{
+    public static UIManagerAvantProces singleton;
+
+    private void Awake()
+    {
+        if (singleton)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            singleton = this;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ProofDisplayUpdate(int currentProofIndex)
+    {
+        ProofManager proofManager = ProofManager.singleton;
+        int roundedAxis = Mathf.RoundToInt(Input.GetAxis("Horizontal"));
+        proofManager.proofList[currentProofIndex].SetActive(true);
+        if (currentProofIndex == 0 && roundedAxis == -1)
+        {
+            proofManager.proofList[proofManager.proofList.Length-1].SetActive(false);
+        }
+        else if(currentProofIndex == proofManager.proofList.Length-1 && roundedAxis == 1)
+        {
+            proofManager.proofList[0].SetActive(false);
+        }
+        else
+        {
+            proofManager.proofList[currentProofIndex - roundedAxis].SetActive(false);
+        }
+        
+    }
+}
