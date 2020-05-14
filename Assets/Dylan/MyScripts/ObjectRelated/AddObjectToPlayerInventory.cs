7a79869fccbd47a2ff897839fa70bf4a715f4f1c
Dylan_Schumacker <dylan.schumacker@outlook.fr>
2020-05-14 12:44:59 +0200

    - Correctifs pickup des objets - Débuts sprites UI procès

--- Assets/Dylan/MyScripts/ObjectRelated/AddObjectToPlayerInventory.cs
+++ Assets/Dylan/MyScripts/ObjectRelated/AddObjectToPlayerInventory.cs
@@ -44,26 +44,28 @@ public class AddObjectToPlayerInventory : MonoBehaviour
     //Fonction attachée au clique sur "Oui" de la fenêtre popup de validation d'achat...
     public void AddTheObjectToTheInventory()
     {
-        for (int i = 0; i < PlayerObjectsInventory.s_Singleton.objectsCompartments.Count; i++)
-        {
-            //Si un des compartiments de sort est vide et le joueur n'a pas encore atteint la limite de sort achetable...
-            if (PlayerObjectsInventory.s_Singleton.objectsCompartments[i].GetComponent<ObjectCompartment>().MyCompartmentObject == null)
-            {
-                PlayerObjectsInventory.s_Singleton.numberOfObjectInInventory++;
-
-                //Activation du component image + changement de son sprite du compartiment de sort dans lequel le sort acheté a été ajouté
-                PlayerObjectsInventory.s_Singleton.objectsCompartments[i].GetComponent<ObjectCompartment>().MyCompartmentObject = _object;
-                PlayerObjectsInventory.s_Singleton.objectsCompartments[i].GetComponent<Image>().enabled = true;
-                PlayerObjectsInventory.s_Singleton.objectsCompartments[i].GetComponent<Image>().sprite = _object.MyObjectIcon;
-
-                Destroy(this.gameObject);
-                return;
-            }
-        }
-
         if (PlayerObjectsInventory.s_Singleton.numberOfObjectInInventory == 3)
         {
             StartCoroutine(UIManager.s_Singleton.FadeInAndOutObjectFeedBack(UIManager.s_Singleton.cantPickAnObjectFeedback));
+            return;
+        }
+        else
+        {
+            for (int i = 0; i < PlayerObjectsInventory.s_Singleton.objectsCompartments.Count; i++)
+            {
+                //Si un des compartiments de sort est vide et le joueur n'a pas encore atteint la limite de sort achetable...
+                if (PlayerObjectsInventory.s_Singleton.objectsCompartments[i].GetComponent<ObjectCompartment>().MyCompartmentObject == null)
+                {
+                    PlayerObjectsInventory.s_Singleton.numberOfObjectInInventory++;
+
+                    //Activation du component image + changement de son sprite du compartiment de sort dans lequel le sort acheté a été ajouté
+                    PlayerObjectsInventory.s_Singleton.objectsCompartments[i].GetComponent<ObjectCompartment>().MyCompartmentObject = _object;
+                    PlayerObjectsInventory.s_Singleton.objectsCompartments[i].GetComponent<Image>().enabled = true;
+                    PlayerObjectsInventory.s_Singleton.objectsCompartments[i].GetComponent<Image>().sprite = _object.MyObjectIcon;
+
+                    return;
+                }
+            }
         }
     }
 }
