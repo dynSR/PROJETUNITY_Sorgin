1e1f7bef6f5e1d679213d371977bbc399590808f
Dylan_Schumacker <dylan.schumacker@outlook.fr>
2020-05-22 16:00:11 +0200

    *.*

---
+++ Assets/Dylan/MyScripts/EventSystemHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSystemHandler : MonoBehaviour
+{
+    public void SetFirstObjectOfEventSystem()
+    {
+        Debug.Log("SALUT J'UTILISE LA METHOD DONT T'AS BESOIN");
+    }
+}
